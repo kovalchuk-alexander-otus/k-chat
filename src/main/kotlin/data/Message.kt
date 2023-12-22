18dfe486efdaf03af43edc36a42740bbc869618a
alexander.kovalchuk <alexander.kovalchuk.aka.maki@gmail.com>
2023-12-22 03:11:28 +0300

    add requirements

--- src/main/kotlin/data/Message.kt
+++ src/main/kotlin/data/Message.kt
@@ -7,5 +7,6 @@ data class Message(
     val author: User, // автор сообщения
     var text: String, // текст сообщения
     var date: Long, // дата
-    val onlyOne: Boolean? = false // признак того, что сообщение после прочтения удаляется из чата
+    val onlyOne: Boolean? = false, // признак того, что сообщение после прочтения удаляется из чата
+    var isReading: Boolean = false // признак того, что сообщение прочитано
 )
